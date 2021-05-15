ca6df8930c0576d0d8755a1c3795da856f4da6f8
mujamhmmd <83144135+mujamhmmd@users.noreply.github.com>
2021-05-16 03:47:19 +0700

    Create zcashcoin.sh

---
+++ zcashcoin.sh
@@ -0,0 +1,5 @@
+# zcashcoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u t1gKpJf3mXviEHVftSLAg5FBCVw7rtV9ahi.moozone -p x
